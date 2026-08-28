eb765804d96be526a7a68a8e3b73069f8e53a15c
kentaki <kentaki@users.noreply.github.com>
2026-08-28 22:58:31 +0900

    refactor: 杖クラスに抽象継承メンバーを追加

--- src/features/magic/rods/draugrRod.ts
+++ src/features/magic/rods/draugrRod.ts
@@ -1,5 +1,31 @@
 import { MagicSystem } from "../magicSystem";
 
 export class DraugrRod extends MagicSystem {
-
+  onPlayerClick(...args: any[]): void {
+    
+  }
+  onPlayerClickUp(...args: any[]): void {
+    
+  }
+  onPlayerAltAction(...args: any[]): void {
+    
+  }
+  onAttemptKillPlayer(...args: any[]): void {
+    
+  }
+  onPlayerDropItem(...args: any[]): void {
+    
+  }
+  onPlayerPickedUpItem(...args: any[]): void {
+    
+  }
+  onPlayerSwapInvenSlots(...args: any[]): void {
+    
+  }
+  onTouchscreenActionButton(...args: any[]): void {
+    
+  }
+  tick(...args: any[]): void {
+    
+  }
 }
